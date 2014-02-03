bef5a32ab0f7b17073e64fe19948f8a0f12ba228
vagrant <vagrant@precise64.(none)>
2014-02-03 03:27:58 +0000

    createVMoutofimage.py

---
+++ exercise1/createVMoutofimage.py
@@ -0,0 +1,24 @@
+# To be execuclienit as KeystoneClientted from the host machine (not vagrant VM)
+from keystoneclient.v2_0 import Client as KeystoneClient
+from glanceclient import Client as GlanceClient
+from novaclient.v1_1 import Client as NovaClient
+
+keystone = KeystoneClient(username = 'admin',
+               		  password = 'password',
+                          tenant_name = 'demo',
+                          auth_url = 'http://10.0.2.15:5000/v2.0')
+glance = GlanceClient('1', endpoint = 'http://10.0.2.15:9292',
+			   token = keystone.auth_token)
+images = glance.images.list()
+for image in images:
+	if image.name == 'ubuntu':
+		nova = NovaClient(username = "admin", 
+				  api_key = 'password',  
+				  auth_url = 'http://10.0.2.15:5000/v2.0',
+				  project_id = 'demo')
+		flavor = nova.flavors.find(name='m1.small')
+		nova.servers.create(name = "myubuntu", 
+				    image = image.id,
+				    flavor = flavor)
+
+
